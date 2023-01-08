87629617feeef9f8bb49117d3333c96e1a9c88aa
Elias Torres <eliato_@hotmail.com>
2023-01-07 21:32:00 -0600

    adding card

--- fl_components/lib/screens/card_screen.dart
+++ fl_components/lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen
@@ -9,10 +10,29 @@ class CardScreen
   @override
   Widget
       build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+      title: const Text('Card Wiget'),
       ),
+      body: ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      children: [
+        Card(
+          child: Column(
+
+            children: const[
+              ListTile(
+              leading: Icon(Icons.phone_android, color: AppTheme.primary),  
+              title: Text('titulo card'),
+              subtitle: Text('Anim cupidatat pariatur quis officia anim excepteur pariatur pariatur deserunt Lorem ullamco sint.'),
+              )
+
+              ],
+
+            ),
+          )
+        ],  
+    )
     );
   }
 }
