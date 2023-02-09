f2b6e0a7216a55c07f68952ba1aa19f8f5801704
Elias Torres <eliato_@hotmail.com>
2023-02-08 22:12:59 -0600

    add wiget container animated

--- fl_components/lib/router/app_router.dart
+++ fl_components/lib/router/app_router.dart
@@ -7,12 +7,13 @@ class AppRoutes {
   static const initialRoute = 'home';
 
   static final menuOption = <MenuOption> [
-MenuOption(route: 'home', name: 'Home Screen', screend: const HomeScreen(), icon: Icons.home),
+MenuOption(route: 'home', name: 'Home Screen', screend: const HomeScreen(), icon: Icons.home_outlined),
 MenuOption(route: 'listview1', name: 'List View 1', screend: const Listview1Screen(), icon: Icons.list_alt_outlined),
 MenuOption(route: 'listview2', name: 'List View 2', screend: const Listview2Screen(), icon: Icons.list),
 MenuOption(route: 'alert', name: 'Alerts Screen', screend: const AlertScreen(), icon: Icons.notification_important_outlined),
 MenuOption(route: 'card', name: 'Cards Screen', screend: const CardScreen(), icon: Icons.credit_card),
 MenuOption(route: 'avatar', name: 'Avatar Screen', screend: const AvatarScreen(), icon: Icons.supervised_user_circle_outlined),
+MenuOption(route: 'animated', name: 'Animated Screen', screend: const AnimatedScreen(), icon: Icons.play_circle_outline_rounded),
 
 ];
 
